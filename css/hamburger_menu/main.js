a71e6ed8a21f3ba0a61689c2796b2574ca07ba28
kirikirimai <47017306+kirikirimai@users.noreply.github.com>
2020-08-11 19:02:50 +0900

    修正。 transition をtranslate から　絶対配置のleftでアニメーションさせたほうがシンプルに。

--- css/hamburger_menu/main.js
+++ css/hamburger_menu/main.js
@@ -2,20 +2,20 @@ $(function () {
 
   $(".btn-trigger").on("click", function () {
     $(this).toggleClass("active");
-    
-    if ($("#gnav-list").hasClass("active")) {
+    $("#gnav-list").toggleClass("active");
+    // if ($("#gnav-list").hasClass("active")) {
 
-      $("#gnav-list").removeClass("active");
-      setTimeout(function () {
-        $("#gnav-wrap").removeClass("is-open");
-      }, 500);
+    //   $("#gnav-list").removeClass("active");
+    //   setTimeout(function () {
+    //     $("#gnav-wrap").removeClass("is-open");
+    //   }, 500);
     
-    } else {
-      $("#gnav-wrap").addClass("is-open");
-      setTimeout(function () {
-        $("#gnav-list").addClass("active");
-      }, 5);
-    }
+    // } else {
+    //   $("#gnav-wrap").addClass("is-open");
+    //   setTimeout(function () {
+    //     $("#gnav-list").addClass("active");
+    //   }, 5);
+    // }
     return false;
   });
 });
