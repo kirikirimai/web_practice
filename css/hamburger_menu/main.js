14e9e0732dbcd399724b5fab63fdcd37f9e37530
kirikirimai <47017306+kirikirimai@users.noreply.github.com>
2020-08-11 23:28:43 +0900

    ヘッダーを固定に変更 JSによるヘッダの高さによるナビの調整（window.matchMediaによるメディアクエリの検知）

--- css/hamburger_menu/main.js
+++ css/hamburger_menu/main.js
@@ -1,15 +1,36 @@
 $(function () {
 
+  
+  var mediaQuery = matchMedia('(min-width: 780px)');
+  // ページが読み込まれた時に実行
+  handle(mediaQuery);
+  // ウィンドウサイズが変更されても実行されるように
+  mediaQuery.addListener(handle);
+
+  function handle(mq) {
+    if (mq.matches) {
+      // ウィンドウサイズが780px以上の時
+      console.log("780以上");
+      $("#gnav").css("top","20px");
+      $("#main-contents").css("padding-top",120+"px");
+    } else {
+      // それ以外
+      console.log("780以下");
+       var h = $("#header").outerHeight();
+       $("#gnav").css("top",h+"px");
+       $("#main-contents").css("padding-top",h+20+"px");
+       
+    }
+  }
+  // $("#gnav").css("top",h+"px");
   $(".btn-trigger").on("click", function () {
     $(this).toggleClass("active");
     $("#gnav-list").toggleClass("active");
     // if ($("#gnav-list").hasClass("active")) {
-
     //   $("#gnav-list").removeClass("active");
     //   setTimeout(function () {
     //     $("#gnav-wrap").removeClass("is-open");
     //   }, 500);
-    
     // } else {
     //   $("#gnav-wrap").addClass("is-open");
     //   setTimeout(function () {
