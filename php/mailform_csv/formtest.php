3624de919acaa62df58c932a2c5d542b7dacafa2
kirikirimai <47017306+kirikirimai@users.noreply.github.com>
2020-09-27 13:28:01 +0900

    修正

--- php/mailform_csv/formtest.php
+++ php/mailform_csv/formtest.php
@@ -2,7 +2,7 @@
 <?php
 
 //メールの宛先（To）のメールアドレス
-define('MAIL_TO', "go.revolution.no3@gmail.com");
+define('MAIL_TO', "xxxxx@xxxxx.com");
 //メールの宛先（To）の名前  
 define('MAIL_TO_NAME', "きりやま");
 //Cc の名前
@@ -12,7 +12,7 @@ define('MAIL_CC_NAME', 'Cc宛先名');
 //Bcc
 define('MAIL_BCC', 'xxxxx@xxxxx.com');
 //Return-Pathに指定するメールアドレス
-define('MAIL_RETURN', 'the_zombis@hotmail.co.jp');
+define('MAIL_RETURN', 'xxxxx@xxxxx.com');
 //自動返信の返信先名前（自動返信を設定する場合）
 define('AUTO_REPLY_NAME', '返信先名前');
 
